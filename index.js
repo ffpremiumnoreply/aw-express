4dbd8823f31b459689e49cb17dc389bdcb322e77
Patiwat Numbut <tonmaster.1.08@hotmail.com>
2021-10-16 14:55:23 +0000

    Update limite time

--- index.js
+++ index.js
@@ -227,7 +227,7 @@ async function mine(DATA){
         //  }; 
         
         if (
-            itr >= 100000 * 8 || (end-start) / 1000 >= (60 / (Number(difficulty) || 1))
+            itr >= 100000 * 16 || (end-start) / 1000 >= (20 / (Number(difficulty) || 1))
         ){
             rand_arr    = ''; 
             hex_digest  = `SORRY WE CAN NOT SOLVED LOOP ${ itr }`; 
